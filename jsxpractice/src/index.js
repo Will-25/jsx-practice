d4873e959f2c294da03bd3c46842859b145d33cc
Will <Will.irvin18@gmail.com>
2020-08-22 14:50:31 -0400

    reference function in jsx

--- jsxpractice/src/index.js
+++ jsxpractice/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function getButton() {
+    return 'Click on Me!';
+}
+
 const App = () => {
-    return <div>hi there!</div>;
+    const buttonText = 'Click Me!';
+
+    return (
+        <div>
+            <label className="label" for="name">Enter Name:</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButton()}</button>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
